4ea2cea566feb725f9333494b7106b64178edbd7
kdhqwe1030 <kdhqwe1030@naver.com>
2026-03-16 21:53:54 +0900

    chore: 카테고리 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -233,6 +233,7 @@ export default function Home() {
                     ? "📁"
                     : (sp.categories?.icon ?? "📍")
                 }
+                naverCategory={sp.places.naver_category ?? undefined}
               />
             </button>
           ))
